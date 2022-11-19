effacac4fcb0357ccf842ec1a59940bd65f27054
Hong-ut <7594hsj@gmail.com>
2022-11-19 17:23:05 -0500

    add frontend

--- static/main.js
+++ static/main.js
@@ -8,6 +8,22 @@ var canvas, ctx, flag = false,
 var x = "black",
 		y = 2;
 
+// Advanced options (sliders)
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+	coll[i].addEventListener("click", function() {
+		this.classList.toggle("active");
+		var content = this.nextElementSibling;
+		if (content.style.maxHeight){
+		content.style.maxHeight = null;
+		} else {
+		content.style.maxHeight = content.scrollHeight + "px";
+		}
+	});
+}
+
 function init() {
 		canvas = document.getElementById('can');
 		ctx = canvas.getContext("2d");
